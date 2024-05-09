60f5c2414c1a12887187491bfb04e379925a0589
linjonah <znljonah@gmail.com>
2024-05-09 12:35:01 -0700

    update for task 8

--- badger-mart/jobs.js
+++ badger-mart/jobs.js
@@ -14,6 +14,8 @@ function submitApplication(e) {
             alert("Thank you for applying to be a " + selectedJob);
 
             break;
+        } else if (position === openPositions[openPositions.length - 1]) {
+            alert("Please select a job!");
         }
     }
     // console.log(selectedJob);
